2585bf120bbd5b256af78bf3aaf8cad698c874ec
Tayanovsky Ilya <ilatayanovskij@gmail.com>
2026-07-03 16:11:47 +0300

    fix

--- app/test_main.py
+++ app/test_main.py
@@ -6,12 +6,14 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, human_year",
     [
+        (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
         (23, 23, [1, 1]),
         (24, 24, [2, 2]),
-        (27, 28, [2, 2]),
-        (28, 29, [3, 3])
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
     ]
 )
 def test_get_human_age(cat_age: int, dog_age: int, human_year: list) -> None:
