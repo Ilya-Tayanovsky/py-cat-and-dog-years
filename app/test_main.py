8d15b81b97db09405ae6711e81be35b7d8659f9a
Tayanovsky Ilya <ilatayanovskij@gmail.com>
2026-07-07 11:35:49 +0300

    fix2

--- app/test_main.py
+++ app/test_main.py
@@ -6,6 +6,7 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, human_year",
     [
+        (-1, -10, [0, 0]),
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
@@ -13,8 +14,14 @@ from app.main import get_human_age
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        (100, 100, [21, 17]),
+        (10_000, 5_000, [2_496, 997])
     ]
 )
 def test_get_human_age(cat_age: int, dog_age: int, human_year: list) -> None:
+    if not isinstance(cat_age, int):
+        raise TypeError
+    if not isinstance(dog_age, int):
+        raise TypeError
+
     assert get_human_age(cat_age, dog_age) == human_year
